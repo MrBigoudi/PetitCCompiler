b24d0be467a19be8bdef10670347f3b5ee9274fe
a-ananas <antoine.anastassiades@gmail.com>
2022-11-27 21:26:07 +0100

    updating pretty printer to do TDD

--- compiler/bin/syntax/prettyPrinter.ml
+++ compiler/bin/syntax/prettyPrinter.ml
@@ -5,12 +5,29 @@ open Ast
 
 (* puis plus joliment, en utilisant les boîtes de Format *)
 let rec print fmt = function
-  | Econst n      -> fprintf fmt "%d" n
+  | Econst const  -> print_const const fmt
+  | Eunop (op, e) -> print_unop op fmt e
   | Ebinop (op,e1,e2)   -> print_binop op fmt e1 e2
-  | _ -> fprintf fmt ""
+
+and print_const const fmt = match const with
+  | Int n -> fprintf fmt "%d" n
+  | True  -> fprintf fmt "true"
+  | False -> fprintf fmt "false"
+  | Null  -> fprintf fmt "NULL"
 
 and print_binop op fmt e1 e2 = match op with
   | Badd -> fprintf fmt "(@[%a +@ %a@])" print e1 print e2
-  | Bmul -> fprintf fmt "(@[%a *@ %a@])" print e1 print e2
-  | _ -> fprintf fmt ""
-;;
+  | Bsub -> fprintf fmt "(@[%a -@ %a@])" print e1 print e2
+  (*| Bmul -> fprintf fmt "(@[%a *@ %a@])" print e1 print e2*)
+
+and print_unop op fmt e = match op with
+  | Unot -> fprintf fmt "(@[!%a@])" print e 
+
+let a = Econst(Int(1))
+let b = Econst(True)
+let c = Econst(Null)
+let d = Eunop(Unot, a)
+let e = Ebinop(Badd, b, a)
+let f = Ebinop(Bsub, c, a)
+
+let () = print std_formatter f
