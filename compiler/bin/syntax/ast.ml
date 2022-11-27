5030fd0748def075bb0adc95e98d276a8cc9c942
a-ananas <antoine.anastassiades@gmail.com>
2022-11-27 20:58:13 +0100

    minimalist ast and type tree

--- compiler/bin/syntax/ast.ml
+++ compiler/bin/syntax/ast.ml
@@ -1,80 +1,28 @@
-(* petitC Abstract Syntax Tree *)
 type ident = string
 
+type const = 
+  | Int of int
+  | True
+  | False
+  | Null
 
-and decl_fct =
-  | DFct of ctype * ident * param list * block
+type unop = Unot
 
+type binop = Badd | Bsub
 
-and cfile =
-  | CFinclude of decl_fct list
+(*type expression = {
+  desc: desc;
+  loc: Lexing.position * Lexing.position
+}*)
 
+type expression =
+  | Econst of const 
+  | Eunop of unop * expression
+  | Ebinop of binop * expression * expression
 
-and ctype =
-  | Tvoid
-  | Tint
-  | Tbool
-  | Tptr of ctype
-
-
-and param =
-  | PIdnt of ctype * ident
-
-
-and const =
-  | Cint of int
-  | Cnull
-
-
-and unop =
-  | Uamp   (* & *)
-  | Ustar  (* * *)
-  | Unot   (* ! *)
-
-
-and expr =
-  { desc: desc ;
-    loc : Lexing.position * Lexing.position }
-
-and desc =
-  | Econst of const (*constant*)
-  | Eident of ident
-  (* | Eptr of *)
-  | Eassign of expr * expr
-  | Ecall of ident * expr list
-  | Ebinop of binop * expr * expr
-  | Eunop of unop * expr 
-  (* | Esize of  *)
-
-
-and binop =
-  | Badd | Bsub | Bmul | Bdiv | Bmod    (* + - * / % *)
-  | Beq | Bneq | Blt | Ble | Bgt | Bge  (* == != < <= > >= *)
-  | Band | Bor                          (* && || *)
-
-
-and instr =
-  | Iempt
-  | Iexpr of expr
-  | Iif of expr * instr * instr
-  | Iwhile of expr * instr
-  | Ifor of decl_var option * expr option * expr list * instr
-  | Iblock of block
-  | Iret of expr option
-  | Ibrk
-  | Icontinue
-
-
-
-and decl_instr =
-  | DIvar of decl_var
-  | DInstr of instr
-  (* option Dfct of decl_fct *)
-
-
-and block =
-  | Blck of decl_instr list
-
-
-and decl_var =
-  | DVar of ctype * ident * expr option
+let a = Econst(Int(1))
+let b = Econst(True)
+let c = Econst(Null)
+let d = Eunop(Unot, a)
+let e = Ebinop(Badd, b, a)
+let f = Ebinop(Bsub, c, a)
--- compiler/bin/syntax/typer.ml
+++ compiler/bin/syntax/typer.ml
@@ -1,18 +1,29 @@
-(* petitC Typer *)
-
 open Ast
 
 type typ = 
-  | Tvoid
-  | Tint
+  | Tint 
   | Tbool
+  | Tvoid
   | Tptr of typ
 
 module Smap = Map.Make(String)
-
 type env = typ Smap.t
 
-let rec type_expr env = function
-  | Econst Cint _ -> Tint
-  | Econst Cnull  -> Tptr(Tvoid)
-  (* true and false does not exist !! *)
+(* lvalue : variable ou *e1 *)
+
+let rec type_expr (*env*) =  function
+  | Econst Null -> Tptr(Tvoid)
+  | Econst _ -> Tint (* dunno if its more subtle but lets do this way *)
+  | Eunop (Unot, c) -> (* NULL is of type void* and !NULL of type int *)
+      if type_expr c = Tvoid then failwith "erreur : invalid use of void expression"
+      else Tint
+  | Ebinop (_, _, _) -> Tint (* addition of pointers dont work as expected, be more careful ! *)
+
+let a = type_expr (Econst(Int(1)))
+let a'= Econst(Int(1))
+let b = type_expr (Econst(True))
+let b'= Econst(True)
+let c = type_expr (Econst(Null))
+let c'= Econst(Null)
+let e = type_expr (Ebinop(Badd, b', a'))
+let f = type_expr (Ebinop(Bsub, c', a'))
